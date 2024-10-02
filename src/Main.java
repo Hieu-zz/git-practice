342f8a4fa64f68d45d4fb6b3d5cae91d1b379ba7
KinggKong <134825851+KinggKong@users.noreply.github.com>
2024-10-02 09:30:42 +0700

    feature tinh hcn

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Width: ");
+        double width = scanner.nextDouble();
+        System.out.println("Height: ");
+        double height = scanner.nextDouble();
+        double dienTich = width * height;
+        double chuVi = (width + height) * 2;
+        System.out.println("Dien tich nguyen la: " + (int) dienTich);
+        System.out.println("Chu vi nguyen la: " + (int) chuVi);
+        System.out.println("Dien tich thuc la:  " +  String.format("%.3f", dienTich));
+        System.out.println("Chu vi thuc la: " +String.format("%.3f", chuVi));
+    }
+}
