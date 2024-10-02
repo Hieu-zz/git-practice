c67e88f1d128222c0cc449c60c92fdb7bfa2e79b
trdquang <trrdquang@gmail.com>
2024-10-02 11:24:58 +0700

    up

---
+++ loop/Task2/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = sc.nextInt();
+        System.out.print("b = ");
+        int b = sc.nextInt();
+        System.out.print("phép tính: ");
+        char c = sc.next().charAt(0);
+        if(c == '+')
+            System.out.printf("%d + %d = %d", a, b, (a+b));
+        else if (c == '-')
+            System.out.printf("%d - %d = %d", a, b, (a-b));
+        else if (c == '*')
+            System.out.printf("%d * %d = %d", a, b, (a*b));
+        else if (c == '/')
+            if(b == 0)
+                System.out.println("Không hợp lệ");
+            else
+                System.out.printf("%d / %d = %.2f", a, b, ((a*1.0)/b));
+        else
+            System.out.println("Phép tính không đúng");
+    }
+}
