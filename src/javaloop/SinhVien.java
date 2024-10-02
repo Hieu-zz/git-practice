9f6f03072be93d0157787abcdcc0e518bc509ca6
KinggKong <134825851+KinggKong@users.noreply.github.com>
2024-10-02 15:32:43 +0700

    add class sinh vien

---
+++ src/javaloop/SinhVien.java
@@ -0,0 +1,39 @@
+package javaloop;
+
+public class SinhVien {
+    String name, className;
+    int age;
+    String dob;
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
