b0ee2424f248f77e84b6a378d0e15b5cd68f854a
sonvt <vuthanhson041995@gmail.com>
2024-10-02 10:38:53 +0700

    bai 2

---
+++ Java_loop/Test2.java
@@ -0,0 +1,29 @@
+package Java_loop;
+
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi nhap so nguyen thu nhat");
+        a = sc.nextInt();
+        System.out.println("Moi nhap so nguyen thu hai");
+        b = sc.nextInt();
+        System.out.println("Moi nhap so nguyen thu ba");
+        c = sc.nextInt();
+        int max = a;
+        if (max < b) {
+            max = b;
+        }
+        if (max < c) {
+            max = c;
+        }
+
+        System.out.println("So lon nhat " + max);
+        if (max == b || max == c || b == c) {
+            System.out.println("So lon nhat bang nhau");
+        }
+
+    }
+}
--- test1.java
+++ test1.java
@@ -16,9 +16,11 @@ public class test1 {
         int P = (int)p;
         System.out.println("dien tich hcn sau khi chuyen doi " + S);
         System.out.println("chu vi hcn sau khi chuyen doi " + P);
-        System.out.format("%.3f", s);
+        System.out.printf("%.3f", s);
         System.out.format("%.3f",p );
         //coment fdhgdsugjfkdhj
+
+
 
 
 
