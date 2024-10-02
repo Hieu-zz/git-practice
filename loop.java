4a80fa7349301915491bfcc91d7f8482a5338512
itsHuaan <doquanghuan1909@gmail.com>
2024-10-02 11:02:00 +0700

    Tim so lon nhat

---
+++ loop.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    int firstNum, secondNum, thirdNum;
+    try (Scanner input = new Scanner(System.in)) {
+        System.out.print("Nhap so thu 1: ");
+        firstNum = input.nextInt();
+        System.out.print("Nhap so thu 2: ");
+        secondNum = input.nextInt();
+        System.out.print("Nhap so thu 3: ");
+        thirdNum = input.nextInt();
+    }
+    list.add(firstNum);
+    list.add(secondNum);
+    list.add(thirdNum);
+    int max = firstNum;
+    int count = 0;
+    for (int num : list) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    if (count > 1) {
+        System.out.println("So lon nhat bang nhau: " + max);
+    } else {
+        System.out.println("So lon nhat: " + max);
+    }
+}
