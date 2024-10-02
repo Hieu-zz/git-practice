ee5221c52e209264efd6a0f815b8dd6b92cac567
trdquang <trrdquang@gmail.com>
2024-10-02 13:48:11 +0700

    up

---
+++ loop/untitled/src/task1_dx/Main.java
@@ -0,0 +1,22 @@
+package task1_dx;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("s = ");
+        String s = sc.nextLine();
+
+        int l = 0, r = s.length()-1;
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)){
+                System.out.println("Không đối xứng");
+                return;
+            }
+            l++;
+            r--;
+        }
+        System.out.println("Đối xứng");
+    }
+}
